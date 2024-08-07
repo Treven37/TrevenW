cf725462d6c611d8e1f23c8894cfff0c8513774e
treven37 <officialtreven37@gmail.com>
2024-08-07 22:15:02 +0800

    changes app.jsx content

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import trevenLogo from './assets/treven.svg'
+
+import './App.scss'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,14 +10,14 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
+          <img src={trevenLogo} className="logo react" alt="React logo" />
+        </a>
+		<a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Treven + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
