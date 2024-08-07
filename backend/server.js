e9c9b8585bb8b920e296f1bd09036d2a4de3c8fe
treven37 <officialtreven37@gmail.com>
2024-08-07 08:04:58 +0800

    added server, connected to mongoose and mongo database

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,37 @@
-const initDotenv = require('dotenv').config();
+require('dotenv').config();
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.json();
+});
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const mongoDBUri = process.env.MONGO_URI;
+
+mongoose.connect(mongoDBUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Failed to connect to MongoDB', err));
+const db = mongoose.connection;
+db.on('connected', () => console.log('Mongoose connected'));
+db.on('error', err => console.error('Mongoose connection error:', err));
+db.on('disconnected', () => console.log('Mongoose disconnected'));
+
+app.listen(PORT, () => {
+	console.log(`App listening at port ${PORT}`)
+});
+
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('Mongoose connection disconnected through app termination');
+    process.exit(0);
+  });
+});
